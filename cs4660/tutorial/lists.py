e13eb38645f1af79c1f92aaca0e85aff418a32f0
Matthew <mattacoo@gmail.com>
2017-09-05 19:47:45 -0700

    first

--- cs4660/tutorial/lists.py
+++ cs4660/tutorial/lists.py
@@ -2,20 +2,27 @@
 
 def get_first_item(li):
     """Return the first item from the list"""
+    return li[0]
     pass
 
 def get_last_item(li):
     """Return the last item from the list"""
-    pass
+    return li[-1]
+
 
 def get_second_and_third_items(li):
     """Return second and third item from the list"""
-    pass
+    return li[1:3]
 
 def get_sum(li):
     """Return the sum of the list items"""
+    sum = 0
+    for number in li:
+        sum += number
+    return sum
     pass
 
 def get_avg(li):
     """Returns the average of the list items"""
+    return get_sum(li)/len(li)
     pass
