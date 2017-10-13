4dede56ebbb1a861fdbbb3e0bc73bfb20b26981d
Matthew <mattacoo@gmail.com>
2017-10-12 23:09:19 -0700

    BFS works for all except last element

--- cs4660/search/searches.py
+++ cs4660/search/searches.py
@@ -2,13 +2,39 @@
 Searches module defines all different search algorithms
 """
 from graph import graph as gr
+from queue import *
 def bfs(graph, initial_node, dest_node):
     """
     Breadth First Search
     uses graph to do search from the initial_node to dest_node
     returns a list of actions going from the initial node to dest_node
     """
-    
+    nodes = []
+    parents = {}
+    actions = []
+    end_tile = ((None, None, None))
+    q = Queue()
+    q.put((initial_node, 0, None))
+    while(q.qsize() > 0):
+        u = q.get()
+        for node in graph.neighbors(u[0]):
+            if node not in nodes:
+                if node == dest_node:
+                    end_tile = u
+                nodes.append(node)
+                q.put((node, graph.distance(u[2], node), u))
+    while (end_tile[2] is not None):
+        actions.append(gr.Edge(end_tile[2][0], end_tile[0], graph.distance(end_tile[2][0], end_tile[0])))
+        end_tile = end_tile[2]
+    actions.reverse()
+   # print("\n" + str(actions))
+    #actions.append(gr.Edge(actions[len(actions)-1].to_node, dest_node, graph.distance(actions[len(actions)-1].to_node, dest_node)))
+    print("\n" + str(actions))
+    return actions
+def bfs_help(graph, start, parents):
+    #for node in graph:
+    pass
+
 def dfs(graph, initial_node, dest_node):
     """
     Depth First Search
